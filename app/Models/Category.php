976d1a71691228f13477e6aeb73979cc16dd5e97
Abigail Larupay <gail.larupay@gmail.com>
2024-03-11 11:28:34 +0800

    added parent_id on migration (late add)

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,8 +9,10 @@ class Category extends Model {
 
    use HasFactory;
 
-   protected $fillable = ['title','description'];
+   protected $fillable = ['title','description','parent_id'];
 
 }
+
+
 
 ?>
